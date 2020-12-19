f4b4b3803f4ee384935eb4169a51d8b2af0e3524
Rowan Hill <rowan.hill@gmail.com>
2020-12-19 17:49:21 +0000

    Day 19 tidy up

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -46,7 +46,7 @@ impl Rule {
     }
 }
 
-fn main() {
+fn parse_rules() -> HashMap<String, Rule> {
     let rules_reader = BufReader::new(File::open("rules.txt")
         .expect("Cannot open input"));
 
@@ -59,18 +59,29 @@ fn main() {
         rule_map.insert(id.to_string(), rule);
     }
 
+    rule_map
+}
+
+fn count_valid_lines(validator: Box<dyn Fn(&String) -> bool>) -> usize {
+    let messages_reader = BufReader::new(File::open("messages.txt")
+        .expect("Cannot open input"));
+    messages_reader.lines()
+        .map(|line| line.unwrap())
+        .filter(validator)
+        .count()
+}
+
+fn create_matches_rule_zero(rule_map: &HashMap<String, Rule>) -> Box<dyn Fn(&String) -> bool> {
     let rule_zero = rule_map.get("0").unwrap();
     let pattern = format!("\\A{}\\z", rule_zero.to_pattern(&rule_map));
     let regex = Regex::new(&pattern).unwrap();
 
-    let messages_reader = BufReader::new(File::open("messages.txt")
-        .expect("Cannot open input"));
-    let num_valid = messages_reader.lines()
-        .map(|line| line.unwrap())
-        .filter(|line| regex.is_match(line))
-        .count();
-    println!("Part 1: {}", num_valid);
+    Box::new(move |line| {
+        regex.is_match(&line)
+    })
+}
 
+fn create_matches_recursive_eight_and_eleven(rule_map: &HashMap<String, Rule>) -> Box<dyn Fn(&String) -> bool> {
     let rule_42 = rule_map.get("42").unwrap();
     let pattern_42 = format!("\\A{}", rule_42.to_pattern(&rule_map));
     let re_42 = Regex::new(&pattern_42).unwrap();
@@ -78,25 +89,33 @@ fn main() {
     let pattern_31 = format!("\\A{}", rule_31.to_pattern(&rule_map));
     let re_31 = Regex::new(&pattern_31).unwrap();
 
-    let messages_reader = BufReader::new(File::open("messages.txt")
-        .expect("Cannot open input"));
-    let num_valid2 = messages_reader.lines()
-        .map(|line| line.unwrap())
-        .filter(|line| line.len() % 8 == 0)
-        .filter(|line| {
-            let mut end = 0;
-            let mut first_count = 0;
-            while let Some(caps) = re_42.captures(&line[end..]) {
-                end += (&caps[0]).len();
-                first_count += 1;
-            }
-            let mut second_count = 0;
-            while let Some(caps) = re_31.captures(&line[end..]) {
-                end += (&caps[0]).len();
-                second_count += 1;
-            }
-            end == line.len() && first_count > second_count && second_count > 0
-        })
-        .count();
+    Box::new(move |line| {
+        let mut end = 0;
+
+        let mut first_count = 0;
+        while let Some(caps) = re_42.captures(&line[end..]) {
+            end += (&caps[0]).len();
+            first_count += 1;
+        }
+
+        let mut second_count = 0;
+        while let Some(caps) = re_31.captures(&line[end..]) {
+            end += (&caps[0]).len();
+            second_count += 1;
+        }
+
+        end == line.len() && first_count > second_count && second_count > 0
+    })
+}
+
+fn main() {
+    let rule_map = parse_rules();
+
+    let matches_rule_zero = create_matches_rule_zero(&rule_map);
+    let num_valid = count_valid_lines(matches_rule_zero);
+    println!("Part 1: {}", num_valid);
+
+    let matches_recursive_eight_and_eleven = create_matches_recursive_eight_and_eleven(&rule_map);
+    let num_valid2 = count_valid_lines(matches_recursive_eight_and_eleven);
     println!("Part 2: {}", num_valid2);
 }
